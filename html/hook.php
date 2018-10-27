c39ff3b69499d9aab965c4bb9d226a409a5d446f
iPhone <bakajaan@yahoo.co.jp>
2018-10-27 14:24:56 +0900

    Update

--- html/hook.php
+++ html/hook.php
@@ -1,5 +1,17 @@
 <?php
 
-`cd /home/git/webserver; git pull`;
+// 設定
+$LOG_FILE = dirname(/home/git/webserver).'/hook.log';
+$SECRET_KEY = 'hoge';　//githubで設定するsecretもところ
 
-?>
+$header = getallheaders();
+$hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $SECRET_KEY);
+if ( isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac ) {
+    $payload = json_decode($HTTP_RAW_POST_DATA, true);  // 受け取ったJSONデータ
+    // ここに実行したいコードを書く(testレポに移動して、git pullする)
+    exec('cd /home/git/webserver ; git pull');
+    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
+} else {
+    // 認証失敗
+    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
+}
