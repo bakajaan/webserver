f403b992bc9768d0edd61bb6a2e9c14f861011b3
ipad <bakajaan@yahoo.co.jp>
2018-10-27 13:13:22 +0900

    Update key

--- html/hook.php
+++ html/hook.php
@@ -1,9 +1,9 @@
 <?php
 
 $LOG_FILE = dirname(__FILE__).'/hook.log';
-$SECRET_KEY = '** secret **';
+$SECRET_KEY = '3453';
 
-if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload']) ) {
+if ( isset($_GET['key']) && $_GET['key'] === $SECRET_Kisset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
     if ($payload['ref'] === 'refs/heads/master') {
         `cd /home/git/webserver; git pull origin master`;
