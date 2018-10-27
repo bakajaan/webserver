5c0c07e0cdda609d3160e715c15c326ad95b7d29
ipad <bakajaan@yahoo.co.jp>
2018-10-27 12:49:38 +0900

    Fix pho

--- webfile/hook.php
+++ webfile/hook.php
@@ -6,7 +6,7 @@ $SECRET_KEY = '** secret **';
 if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
     if ($payload['ref'] === 'refs/heads/master') {
-        `cd /home/git/web.git; git pull origin master`;
+        `cd /home/git/webserver; git pull origin master`;
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
     }
 } else {
