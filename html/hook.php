63b19440c51860f371da8beb85cf4d7fe0c9642b
ipad <bakajaan@yahoo.co.jp>
2018-10-27 13:55:32 +0900

    Clear

--- html/hook.php
+++ html/hook.php
@@ -1,12 +1,5 @@
 <?php
 
-$SECRET_KEY = 3453;
-
-if ( isset($_GET['key']) && $_GET['key'] === $SECRET_Kisset($_POST['payload']) ) {
-    $payload = json_decode($_POST['payload'], true);
-    if ($payload['ref'] === 'refs/heads/master') {
-        `cd /home/git/webserver; git pull`;
-    }
-};
+`cd /home/git/webserver; git pull`;
 
 ?>
