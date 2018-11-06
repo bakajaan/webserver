c3a112493314f98221079ac668f6f702694d44ad
bakajaan <lion.exile@gmail.com>
2018-11-06 22:25:41 +0900

    update

--- html/js/top.js
+++ html/js/top.js
@@ -1,5 +1,11 @@
-$(function() {
-  $('.user').animate({
-    'marginLeft': '200px'
-  },1000);
+
+$(function(){
+    setTimeout(function(){
+        $("#user").animate({'marginLeft': '30%','height': '20%'},1000);
+    },500);
+});
+$(function(){
+    setTimeout(function(){
+        $("#hide").animate({ opacity: '0'},1000);
+    },1500);
 });
