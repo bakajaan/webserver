a48a5cf5efc5a50783ff9db1a15eebc29455a57b
ipad <bakajaan@yahoo.co.jp>
2018-11-08 14:58:43 +0900

    Update

---
+++ html/wiki/parts.js
@@ -0,0 +1,11 @@
+ $(function() {
+ $("#header").load("../parts/header.html");
+});
+
+ $(function() {
+ $("#topic").load("../parts/topic.html");
+});
+
+ $(function() {
+ $("#item").load("../parts/item.html");
+});
